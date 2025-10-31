2d8638d0efae60c390ca02413fad7da753e3bd30
sunsheng123 <VPT0028@globalymc.com>
2025-10-31 08:07:44 +0800

    Add files via upload

--- LDAS0300.sql
+++ LDAS0300.sql
@@ -2,7 +2,7 @@
 --    @SEE << Valid Common/Item Validity Check >>
 --    @ID      : LDAS0300
 --
---    @Written : 1.0.0                   2025.10.14 Sun Sheng / YMSL
+--    @Written : 1.0.0                   2025.10.14 Sun Sheng / YMSLX
 --    --------------------------------------------------------------------------
 --
 --    @Version : 1.0.0
